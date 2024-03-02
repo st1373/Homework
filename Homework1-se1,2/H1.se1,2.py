f9ca0861771390741dc9044ad2e527d3fd347123
yasin <yasin.lachiny@gmail.com>
2024-03-02 16:41:32 +0100

    second commit

--- Homework1-se1,2/H1.se1,2.py
+++ Homework1-se1,2/H1.se1,2.py
@@ -1,14 +1,14 @@
 ##--Q1
-# def count_letters(input_str):
-#     uppercase_count = 0
-#     lowercase_count = 0
-#     for char in input_str:
-#         if char.isupper():
-#             uppercase_count += 1
-#         elif char.islower():
-#             lowercase_count += 1
-#     print("Uppercase letters:", uppercase_count)
-#     print("Lowercase letters:", lowercase_count)
+def count_letters(input_str):
+    uppercase_count = 0
+    lowercase_count = 0
+    for char in input_str:
+        if char.isupper():
+            uppercase_count += 1
+        elif char.islower():
+            lowercase_count += 1
+    print("Uppercase letters:", uppercase_count)
+    print("Lowercase letters:", lowercase_count)
 
 # input_string = input("Enter a string: ")
 # count_letters(input_string)
